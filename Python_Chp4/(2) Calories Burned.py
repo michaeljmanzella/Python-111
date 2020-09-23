2cfd30aef4af9febe5e50fa7287914a5926362bd
michaeljmanzella <michaeljmanzella@gmail.com>
2020-09-22 19:23:11 -0700

    Update (2) Calories Burned.py

--- Python_Chp4/(2) Calories Burned.py
+++ Python_Chp4/(2) Calories Burned.py
@@ -1,3 +1,5 @@
+# Michael Manzella
+# 22-Sep-20
 # Calories Burned
 # 
 # Running on a particular treadmill you burn 4.2 calories per minute. Write a program that uses a loop to display the number of
